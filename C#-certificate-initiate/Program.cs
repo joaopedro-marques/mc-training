7058651dcb0fef046d09b1dd09acc2ef05ef4835
joaopedro-marques <joaopedro-marques@users.noreply.github.com>
2025-11-09 13:21:58 -0300

    Starting MSLearning path

---
+++ C#-certificate-initiate/Program.cs
@@ -0,0 +1,47 @@
+﻿// See https://aka.ms/new-console-template for more information
+// Console.WriteLine("Hello, World!");
+
+// Console.Write("Congratulations!");
+// Console.Write(" ");
+// Console.Write("You wrote your first line of code!");
+
+
+// Console.WriteLine("Congratulations!");
+// Console.Write("You Wrote your first lines of code.");
+
+// Console.WriteLine("This is the first Line!");
+// Console.WriteLine("This is the second line!");
+
+// Console.WriteLine('b');
+
+// Console.WriteLine('Hello World!');
+
+// Console.WriteLine(123);
+// Console.WriteLine(2.625);
+// Console.WriteLine(12.39816m);
+// Console.WriteLine(true);
+// Console.WriteLine(false);
+// Console.WriteLine("123");
+// Console.WriteLine(123);
+
+// Console.WriteLine("true");
+// Console.WriteLine(true);
+
+// string firstName;
+// firstName = "Jhonny";
+// Console.WriteLine(firstName);
+// firstName = "Liem";
+// Console.WriteLine(firstName);
+// firstName = "Isabella";
+// Console.WriteLine(firstName);
+// firstName = "Yasmin";
+// Console.WriteLine(firstName);
+
+// string firstName;
+// Console.WriteLine(firstName);
+
+string firstName = "Bob";
+int num = 3;
+double y = 34.4;
+
+Console.WriteLine($"Hello {firstName}! You have {num} messages and the temperature is {y} Celsius");
