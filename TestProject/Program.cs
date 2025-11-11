f4d29b4ac2da0e7ac28d847abe4193a75fac650b
joaopedro-marques <joaopedro-marques@users.noreply.github.com>
2025-11-11 11:02:00 -0300

    Working with methods, oveloard, class library

--- TestProject/Program.cs
+++ TestProject/Program.cs
@@ -1,2 +1,26 @@
-﻿// See https://aka.ms/new-console-template for more information
-Console.WriteLine("Hello C#!");
+﻿// // Random dice = new Random();
+// // int roll = dice.Next();
+// // Console.WriteLine(roll);
+
+// // int number = 7;
+// // string text = "seven";
+
+// // Console.WriteLine(number);
+// // Console.WriteLine();
+// // Console.WriteLine(text);
+// Random dice = new Random();
+// int roll1 = dice.Next();
+// int roll2 = dice.Next(101);
+// int roll3 = dice.Next(50, 101);
+
+
+// Console.WriteLine($"First roll: {roll1}");
+// Console.WriteLine($"Second roll: {roll2}");
+// Console.WriteLine($"Third roll: {roll3}");
+
+int firstValue = 500;
+int secondValue = 600;
+int largervalue;
+
+largervalue = Math.Max(firstValue, secondValue);
+Console.WriteLine(largervalue);
