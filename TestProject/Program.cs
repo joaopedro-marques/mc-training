3e48d1aef851a174dadce13d86471a2e406316e0
joaopedro-marques <joaopedro-marques@users.noreply.github.com>
2025-11-12 21:51:46 -0300

    Working with Array and foreach

--- TestProject/Program.cs
+++ TestProject/Program.cs
@@ -105,3 +105,51 @@ if (daysUntilExpiration <= 10)
     Console.WriteLine("You subscriptions will expire soon. Renew Now!");
   }
 }
+
+Console.WriteLine("--------");
+
+string[] fraudulentOrdersIDs = new string[3];
+
+fraudulentOrdersIDs[0] = "A123";
+fraudulentOrdersIDs[1] = "B456";
+fraudulentOrdersIDs[2] = "C789";
+
+// fraudulentOrdersIDs[3] = "D000";
+
+Console.WriteLine($"First: {fraudulentOrdersIDs[0]}");
+Console.WriteLine($"First: {fraudulentOrdersIDs[1]}");
+Console.WriteLine($"First: {fraudulentOrdersIDs[2]}");
+
+fraudulentOrdersIDs[0] = "F000";
+
+Console.WriteLine($"Resigned first: {fraudulentOrdersIDs[0]}");
+
+string[] fraudulentOrdersIDs1 = ["A123", "B456", "F789"];
+
+Console.WriteLine($"First: {fraudulentOrdersIDs1[0]}");
+Console.WriteLine($"First: {fraudulentOrdersIDs1[1]}");
+Console.WriteLine($"First: {fraudulentOrdersIDs1[2]}");
+
+Console.WriteLine($"There are {fraudulentOrdersIDs.Length} fraudulent orders to process.");
+
+int[] inventory = { 200, 300, 450, 700, 175, 250 };
+int sum = 0;
+int bin = 0;
+foreach (int item in inventory)
+{
+  bin++;
+  Console.WriteLine($"Bin {bin} = {item} items (Running total: {sum})");
+  sum += item;
+}
+
+Console.WriteLine($"We have {sum} items in the inventory.");
+
+Console.WriteLine("------------");
+Console.WriteLine("Challenge array interaction");
+
+string[] orders = ["B123", "C234", "A345", "C15", "B177", "G3003", "C235", "B179"];
+foreach(string order in orders)
+{
+  if (order.StartsWith('B'))
+    Console.WriteLine(order);
+}
