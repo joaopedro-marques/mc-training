7967ffb994986215b9191f808d45e289c88cf541
joaopedro-marques <joaopedro-marques@users.noreply.github.com>
2026-02-04 14:52:32 -0300

    Calculadora de conversão de tipos. Peso, temperatura e distância

---
+++ Diario.md
@@ -0,0 +1,5 @@
+03/03/2026
+Projeto calculadora de conversões
+- Tive dificuldades em ler o input do usuário, não sei porque.
+04/02/2025
+- Não sabia como fazer input e logo depois ler oo input do usuário no C#
---
+++ calculadora-project/Program.cs
@@ -0,0 +1,85 @@
+﻿using System.Runtime.InteropServices;
+
+Console.WriteLine("=== CONVERSOR COMPLETO ===");
+
+while(true) {
+    Console.WriteLine("\nOpções:");
+    Console.WriteLine("1. Temperatura (C↔F)");
+    Console.WriteLine("2. Distância (m↔ft)");
+    Console.WriteLine("3. Peso (kg↔lb)");
+    Console.WriteLine("4. Sair");
+    Console.WriteLine("Escolha a opção:");
+
+    string? escolha = Console.ReadLine();
+    
+    if (escolha == "4")
+    {
+      Console.WriteLine("Até mais!");
+      break;
+    }
+        
+    
+    if (escolha == "1"){
+        Console.WriteLine("Converter para (C ou F)?");
+        string tipo = Console.ReadLine();
+        float valor = float.Parse(Input("Valor: "));
+        if (tipo == "C")
+        {
+          valor = (valor -32) * 5/9;
+          Console.WriteLine($"Temperatura é {valor} ºC. ");
+        }
+        else
+        {
+          valor = (valor * 5/9) + 32;
+          Console.WriteLine($"Temperatura é {valor} ºF");
+        }
+
+        string continuar = Input("Escolha: 1- Sair 2- Continuar convertendo");
+        if (continuar == "2")  {Console.WriteLine("Continuando");} else {Console.WriteLine("Até logo"); break;} 
+     }
+
+       if (escolha == "2"){
+        Console.WriteLine("Converter para (m ou ft)?");
+        string tipo = Console.ReadLine();
+        float valor = float.Parse(Input("Valor: "));
+        if (tipo == "m")
+        {
+          valor = valor / 3.281f;
+          Console.WriteLine($"A Metragem é {valor} . ");
+        }
+        else
+        {
+          valor = valor * 3.281f;
+          Console.WriteLine($"Pés é {valor} ");
+        }
+
+        string continuar = Input("Escolha: 1- Sair 2- Continuar convertendo");
+        if (continuar == "2")  {Console.WriteLine("Continuando");} else {Console.WriteLine("Até logo"); break;} 
+     }
+
+     if (escolha == "3"){
+        Console.WriteLine("Converter para (kg ou lb)?");
+        string tipo = Console.ReadLine();
+        float valor = float.Parse(Input("Valor: "));
+        if (tipo == "kg")
+        {
+          valor = valor / 2.205f;
+          Console.WriteLine($"O peso é {valor} lbs. ");
+        }
+        else
+        {
+          valor = valor * 2.205f;
+          Console.WriteLine($"O peso é {valor} kgs");
+        }
+
+        string continuar = Input("Escolha: 1- Sair 2- Continuar convertendo");
+        if (continuar == "2")  {Console.WriteLine("Continuando");} else {Console.WriteLine("Até logo"); break;} 
+     }
+ 
+  }
+
+    static string Input(string prompt)
+    {
+        Console.Write(prompt);
+        return Console.ReadLine();
+    }
