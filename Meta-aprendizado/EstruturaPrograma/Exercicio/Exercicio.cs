a12ed30a63bed88ff3607348771e86f5e242960f
joaopedro-marques <joaopedro-marques@users.noreply.github.com>
2026-02-23 09:15:19 -0300

    Primeiro commit do projeto WebAssembly

---
+++ Meta-aprendizado/EstruturaPrograma/Exercicio/Exercicio.cs
@@ -0,0 +1,10 @@
+// namespace EstruturaPrograma.Exercicio
+// {
+//   public class Program
+//   {
+//     // public static void Main()
+//     // {
+//     //   Console.WriteLine("Exercicio");
+//     // }
+//   }
+// }
---
+++ Meta-aprendizado/EstruturaPrograma/Program.cs
@@ -0,0 +1,25 @@
+﻿using System.Threading.Tasks;
+
+namespace EstruturaPrograma
+{
+     partial class Program
+    {
+        static async Task Main(string[] args)
+        {
+            // Seu código de top-level statement aqui
+            Console.WriteLine("Hello World!");
+            await Task.CompletedTask;
+            
+        }
+    }
+
+    partial class Program
+    {
+        
+    }
+
+    class Auxiliar
+    {
+        // teste
+    }
+}
